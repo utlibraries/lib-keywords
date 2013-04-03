14e7a403efc26d751fc3b0edfce91fbd9191a035
M Rangel <mrangel@austin.utexas.edu>
2013-04-03 16:48:09 -0500

    Initial commit

---
+++ concepts.php
@@ -0,0 +1,85 @@
+<?php include_once('./header.php'); ?>
+
+<script type="text/javascript">
+   function formfocus() {
+      document.getElementById('concept1').focus();
+   }
+   window.onload = formfocus;
+</script>
+
+</head>
+<body>
+
+<?php
+
+//Variables
+$clean = array(); //array for filtered inputs
+$old_concepts = array();
+
+$topic = rtrim($_POST['topic']);
+$topic_id = rtrim($_POST['topic_id']);
+
+if ( isset($topic) && ($topic != "") ) {
+	$topic = htmlentities( $topic, ENT_QUOTES, "UTF-8" );
+	$topic = strip_tags($topic) ;
+	$topic = utf8_decode($topic);
+    $clean['topic'] = $topic ;
+
+	$topic_id = add_topic($clean['topic']) ;
+
+} elseif ( isset($topic_id) && ($topic_id != "") ) {
+	if (!is_numeric($topic_id)) {
+		failure($errmsg['invalidinputs'], $location);
+	}
+
+	$old_concepts = get_topic_concepts($topic_id)  ;
+
+	if ( (isset($old_concepts)) && !(empty($old_concepts)) ) {
+		$delete =  "DELETE FROM libkw_concepts WHERE topic_id='$topic_id'";
+		$resDelete = mysql_query($delete) or die('Query failed: ' . mysql_error());
+	}
+
+} else {
+	failure($errmsg['invalidinputs'], $location);
+}
+
+?>
+
+<h2>How to Generate Keywords</h2>
+
+<h3>List Your Key Concepts</h3>
+
+<form action="keywords1.php" method="POST">
+<input type="hidden" name="topic_id" value="<?php echo $topic_id; ?>">
+<p>Your research topic is:</p>
+<blockquote><?php echo $clean['topic']; ?></blockquote>
+<p>Now, identify 2-4 key concepts within your research topic. Each key concept might be 1-2 words.</p>
+<div class="toggle_area">
+    <div class="toggle_label"><a href="#">See Examples</a></div>
+
+    <div class="toggle_content">
+        <div class="example">For example, let's say your research topic was "Effects of media on women's body image". Your key concepts would be:
+        <ul><li>media</li>
+        <li>women</li>
+        <li>body image</li>
+        </ul>
+        </div>
+    </div>
+</div>
+
+
+
+<p><label for="concept1" class="hidden">Key Concept 1</label><input id="concept1" type="text" name="concept[]" class="dataentry"></p>
+<p><label for="concept2" class="hidden">Key Concept 2</label><input id="concept2" type="text" name="concept[]" class="dataentry"></p>
+<p><label for="concept3" class="hidden">Key Concept 3</label><input id="concept3" type="text" name="concept[]" class="dataentry"></p>
+<p><label for="concept4" class="hidden">Key Concept 4</label><input id="concept4" type="text" name="concept[]" class="dataentry"></p>
+
+<input type="submit" value="Next" class="submitbutton">
+
+</form>
+
+<p>&nbsp;</p>
+
+</body>
+</html>
+
---
+++ keywords1.php
@@ -0,0 +1,126 @@
+<?php include_once('./header.php'); ?>
+
+<script type="text/javascript">
+   function formfocus() {
+      document.getElementById('concept1keyword1').focus();
+   }
+   window.onload = formfocus;
+</script>
+
+</head>
+<body>
+
+<?php
+
+//Variables
+$concepts = array();
+$old_concepts = array();
+$concept_ids = array();
+$tmp = array();
+
+$topic_id = $_POST['topic_id'] ;
+if (!is_numeric($topic_id)) {
+	failure($errmsg['invalidinputs'], $location);
+}
+
+$old_concepts = get_topic_concepts($topic_id)  ;
+
+if ( (isset($old_concepts)) && !(empty($old_concepts)) ) {
+	$delete =  "DELETE FROM libkw_concepts WHERE topic_id='$topic_id'";
+	$resDelete = mysql_query($delete) or die('Query failed: ' . mysql_error());
+}
+
+$concepts = $_POST['concept'];
+
+$num_concepts = 0 ;
+foreach ($concepts as $key => $value) {
+    if ( isset($value) && ($value != "") ) {
+		$value = htmlentities( $value, ENT_QUOTES, "UTF-8" );
+		$value = strip_tags($value) ;
+		$value = utf8_decode($value);
+
+        $id = add_concept($value, $topic_id) ;
+        array_push($concept_ids, $id);
+        $num_concepts++ ;
+    }
+}
+
+if ($num_concepts < 2) {
+	failure($errmsg['toofewconcepts'], $location);
+}
+
+sort($concept_ids) ;
+
+$old_keywords = get_concept_keywords($concept_id)  ;
+
+if ( (isset($old_keywords)) && !(empty($old_keywords)) ) {
+    $delete =  "DELETE FROM libkw_keywords WHERE concept_id='$concept_id[0]'";
+    $resDelete = mysql_query($delete) or die('Query failed: ' . mysql_error());
+}
+
+?>
+
+<h2>How to Generate Keywords</h2>
+
+<h3>Related Keywords for <em>
+
+<?php
+
+    $current_concept_text = get_concept_from_ID($concept_ids[0]) ;
+    echo $current_concept_text;
+
+?>
+
+</em></h3>
+
+<form action="keywords2.php" method="POST">
+<input type="hidden" name="concept_id" value="<?php echo $concept_ids[0]; ?>">
+<input type="hidden" name="topic_id" value="<?php echo $topic_id; ?>">
+<input type="hidden" name="current" value="0">
+
+<p>Your key concepts are:</p>
+<blockquote>
+    <ul>
+
+    <?php
+        foreach ($concept_ids as $c) {
+            $concept_text = get_concept_from_ID($c) ;
+            echo "<li>$concept_text</li>" ;
+        }
+
+    ?>
+    </ul>
+</blockquote>
+<p>Now, try to list at least 3 related keywords for each of your key concepts. These might be synonyms, broader terms, more specific terms, etc.
+<div class="toggle_area">
+    <div class="toggle_label"><a href="#">See Examples</a></div>
+
+    <div class="toggle_content"><br />
+        <div class="example">For example, let's say your key concept was "women". Some related keywords might be:
+        <ul><li>woman - Some authors might choose to use the singular version of women.</li>
+        <li>gender - This is a broader term that might provide a few additional search results.</li>
+        <li>girls - This is a more specific term might provide focus to your search results.</li>
+        </ul>
+        </div>
+    </div>
+</div></p>
+
+<h4>Let's begin with your first key concept, <em><?php echo $current_concept_text; ?></em>:</h4>
+
+<p><label for="concept1keyword1" class="hidden">Keyword 1</label><input id="concept1keyword1" type="text" name="keyword[]" class="dataentry"></p>
+<p><label for="concept1keyword2" class="hidden">Keyword 2</label><input id="concept1keyword2" type="text" name="keyword[]" class="dataentry"></p>
+<p><label for="concept1keyword3" class="hidden">Keyword 3</label><input id="concept1keyword3" type="text" name="keyword[]" class="dataentry"></p>
+<p><label for="concept1keyword4" class="hidden">Keyword 4</label><input id="concept1keyword4" type="text" name="keyword[]" class="dataentry"></p>
+<p><label for="concept1keyword5" class="hidden">Keyword 5</label><input id="concept1keyword5" type="text" name="keyword[]" class="dataentry"></p>
+<p><label for="concept1keyword6" class="hidden">Keyword 6</label><input id="concept1keyword6" type="text" name="keyword[]" class="dataentry"></p>
+<p><label for="concept1keyword7" class="hidden">Keyword 7</label><input id="concept1keyword7" type="text" name="keyword[]" class="dataentry"></p>
+
+<input type="submit" value="Next" class="submitbutton">
+
+</form>
+
+<p>&nbsp;</p>
+
+</body>
+</html>
+
