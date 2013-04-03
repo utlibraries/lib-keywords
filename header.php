15ae1e9cdb4a4d1fc956485bd6d7369420178fd6
unknown <minx@LIB-PCLAX156.austin.utexas.edu>
2013-04-03 14:31:06 -0500

    Initial commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php require_once "./keyword_functions.php"; ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+            "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>How to Generate Keywords</title>
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+<meta name="description" content="How to generate keywords for conducting research.">
+<meta name="keywords" content="instruction services keywords research">
+<meta name="maintainer" content="INSERT MAINTAINER HERE">
+<meta name="package" content="instruction">
+
+<?php
+include('2nd-header.php');
+
+$location='<a href="/students">For Undergraduates</a> > <a href="/students/find/">How to</a> > <a href="index.php">Generate Keywords</a> ';
+
+?>
+
+<link href="keywords.css" rel="stylesheet" type="text/css" />
+
