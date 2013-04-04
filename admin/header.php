151fbce71d0cb54bb426537710630f885c9d44cb
M Rangel <mrangel@austin.utexas.edu>
2013-04-04 15:24:35 -0500

    Initial commit

---
+++ admin/header.php
@@ -0,0 +1,19 @@
+<?php require_once "../keyword_functions.php"; ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+            "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Brainstorming Keywords</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="How to generate keywords for conducting research.">
+<meta name="keywords" content="instruction services keywords research">
+
+<link href="../keywords.css" rel="stylesheet" type="text/css" />
+
+<!--Add extra header tags here-->
+
+</head>
+<body>
+
+<!--Add institution branding here-->
